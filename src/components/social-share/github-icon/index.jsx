69ced3c1be91f89dc731808bcce89f4ee7e18c25
jung-jin-lee <jungjin.lee.dev@gmail.com>
2020-10-10 17:25:17 +0900

    change GithubIcon url

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/jung-jin-lee"
       className="github"
       aria-label="GitHub"
       target="_blank"
