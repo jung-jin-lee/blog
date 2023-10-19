dce3559ae18d8742c4e3acb6707b88532873aa0e
jung-jin-lee <jungjin.lee.dev@gmail.com>
2023-10-19 22:35:51 +0900

    feat: 클립보드 복사 버튼 툴팁 적용

--- components/common/CopyToClipboard.tsx
+++ components/common/CopyToClipboard.tsx
@@ -3,31 +3,42 @@
 import { CopyToClipboard as ReactCopyToClipboard } from 'react-copy-to-clipboard';
 import { ClipboardIcon } from '@heroicons/react/24/outline';
 import { CheckIcon } from '@heroicons/react/20/solid';
-import { useState } from 'react';
+import { useId, useState } from 'react';
+import { Tooltip } from 'react-tooltip';
 
 function CopyToClipboard({ text }: { text: string }) {
+  const id = useId();
   const [isCopied, setIsCopied] = useState(false);
 
   return (
-    <ReactCopyToClipboard
-      text={text}
-      onCopy={(_, result) => {
-        if (result && !isCopied) {
-          setIsCopied(true);
-          setTimeout(() => {
-            setIsCopied(false);
-          }, 3500);
-        }
-      }}
-    >
-      <button
-        aria-label="복사 버튼"
-        className="p-1 border border-md rounded-md border-black bg-white"
+    <>
+      <ReactCopyToClipboard
+        text={text}
+        onCopy={(_, result) => {
+          if (result && !isCopied) {
+            setIsCopied(true);
+            setTimeout(() => {
+              setIsCopied(false);
+            }, 2000);
+          }
+        }}
       >
-        {!isCopied && <ClipboardIcon height={18} />}
-        {isCopied && <CheckIcon color="green" height={18} />}
-      </button>
-    </ReactCopyToClipboard>
+        <button
+          data-tooltip-id={id}
+          aria-label="복사 버튼"
+          className="p-1 border border-md rounded-md border-black bg-white"
+        >
+          {!isCopied && <ClipboardIcon height={18} />}
+          {isCopied && <CheckIcon color="green" height={18} />}
+        </button>
+      </ReactCopyToClipboard>
+      <Tooltip
+        id={id}
+        content="복사되었습니다!"
+        isOpen={isCopied}
+        place="left"
+      />
+    </>
   );
 }
 
